2c0553baf9a763b0b7879756a6cd88cc1d34717d
stsupertam <supmantam@gmail.com>
2017-04-26 23:06:48 +0700

    add collect data file

---
+++ collector.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*- 
+import got 
+import json 
+import codecs
+
+def main(): 
+	
+	def receiveBuffer(tweets):
+		for t in tweets:
+			outputFile.write(('\n%s;%s;%d;%d;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.id)))
+		outputFile.flush();
+		print 'More %d saved on file...\n' % len(tweets)
+		
+		outputFile.close()
+		print 'Done. Output file generated "output_got.csv".'
+
+	tweetCriteria = got.manager.TweetCriteria().setUsername("realDonaldTrump") 
+	outputFile = codecs.open("output.csv", "w+", "utf-8")
+	outputFile.write('username;date;retweets;favorites;text;id')
+	print 'Searching...\n'
+	
+	got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+
+if __name__ == '__main__':
+	main() 
