47d9c288fe2e731861638910a3626161c759d80f
stsupertam <supmantam@gmail.com>
2017-04-27 18:52:04 +0700

    fix global variable bug

--- exporter.py
+++ exporter.py
@@ -2,6 +2,8 @@
 
 import sys,getopt,got,datetime,codecs
 
+i = 0
+
 def main(argv):
 
 	if len(argv) == 0:
@@ -18,16 +20,16 @@ def main(argv):
 
  \nExamples:
  # Example 1 - Get tweets by username [barackobama]
- python Exporter.py --username "barackobama" --maxtweets 1\n
+ python exporter.py --username "barackobama" --maxtweets 1\n
 
  # Example 2 - Get tweets by query search [europe refugees]
- python Exporter.py --querysearch "europe refugees" --maxtweets 1\n
+ python exporter.py --querysearch "europe refugees" --maxtweets 1\n
 
  # Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']
- python Exporter.py --username "barackobama" --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n
+ python exporter.py --username "barackobama" --since 2015-09-10 --until 2015-09-12 --maxtweets 1\n
  
  # Example 4 - Get the last 10 top tweets by username
- python Exporter.py --username "barackobama" --maxtweets 10 --toptweets\n"""
+ python exporter.py --username "barackobama" --maxtweets 10 --toptweets\n"""
 		return
  
 	try:
@@ -61,14 +63,14 @@ def main(argv):
 		
 		outputFile = codecs.open(output, "w+", "utf-8")
 		
-		outputFile.write('username;date;retweets;favorites;text;geo;mentions;hashtags;id;permalink')
+		outputFile.write('username,date,retweets,favorites,text,geo,mentions,hashtags,id,permalink')
 		
 		print 'Searching...\n'
-		
 		def receiveBuffer(tweets):
 			for t in tweets:
-				outputFile.write(('\n%s;%s;%d;%d;"%s";%s;%s;%s;"%s";%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
+				outputFile.write(('\n%s,%s,%d,%d,"%s",%s,%s,%s,"%s",%s' % (t.username, t.date.strftime("%Y-%m-%d %H:%M"), t.retweets, t.favorites, t.text, t.geo, t.mentions, t.hashtags, t.id, t.permalink)))
 			outputFile.flush();
+			global i
 			i += 100
 			print 'More %d saved on file...\n' % len(tweets)
 			print 'Totaltweets %d...\n' % i
@@ -79,7 +81,7 @@ def main(argv):
 		print 'Arguments parser error, try -h' + arg
 	finally:
 		outputFile.close()
-		print 'Done. Output file generated "output_got.csv".'
+		print 'Done. Output file generated "%s".' %output
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
